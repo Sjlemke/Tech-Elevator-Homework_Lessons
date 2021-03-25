4c98a56ef860cb082bcb0345cfd70847200511d3
Samantha Lemke <Sjlemke22@gmail.com>
2021-03-25 16:06:57 -0400

    lesson

--- module-3/11_Introduction_to_Vue_and_Data_Binding/student-exercise/src/components/UserList.vue
+++ module-3/11_Introduction_to_Vue_and_Data_Binding/student-exercise/src/components/UserList.vue
@@ -11,12 +11,12 @@
     </thead>
     <tbody>
       <tr>
-        <td><input type="text" id="firstNameFilter"/></td>
-        <td><input type="text" id="lastNameFilter"/></td>
-        <td><input type="text" id="usernameFilter"/></td>
-        <td><input type="text" id="emailFilter"/></td>
+        <td><input type="text" id="firstNameFilter" v-model="filter.firstName"/></td>
+        <td><input type="text" id="lastNameFilter"  v-model="filter.lasttName" /></td>
+        <td><input type="text" id="usernameFilter"  v-model="filter.username"/></td>
+        <td><input type="text" id="emailFilter"  v-model="filter.emailAddress" /></td>
         <td>
-          <select id="statusFilter">
+          <select id="statusFilter" v-model="filter.status">
             <option value="">Show All</option>
             <option value="Active">Active</option>
             <option value="Disabled">Disabled</option>
@@ -55,23 +55,28 @@ export default {
     },
     computed: {
        filteredList() {
-         return this.users.filter(user=>{
-           const firstName = user.firstName.toLowerCase();
-           const lastName = user.lastName.toLowerCase();
-           const username = user.username.toLowerCase();
-           const emailAddress = user.emailAddress.toLowerCase();
-           const status = user.status.toLowerCase();
-           const mySearch = this.filter;
-
-           return firstName.includes(mySearch.firstName.toLowerCase) ||
-           lastName.includes(mySearch.lasttName.toLowerCase) ||
-           username.includes(mySearch.username.toLowerCase) ||
-           emailAddress.includes(mySearch.emailAddress.toLowerCase) ||
-           status.includes(mySearch.status.toLowerCase) 
-         });
+         let filteredUsers = this.users;
+      if( this.filter.firstName != "" ) {
+        filteredUsers = filteredUsers.filter(user => user.firstName.toLowerCase().includes(this.filter.firstName.toLowerCase()))
       }
+      if( this.filter.lastName != "" ) {
+        filteredUsers = filteredUsers.filter(user => user.lastName.toLowerCase().includes(this.filter.lastName.toLowerCase()))
+      }
+      if( this.filter.username != "" ) {
+        filteredUsers = filteredUsers.filter(user => user.username.toLowerCase().includes(this.filter.username.toLowerCase()))
+      }
+      if( this.filter.emailAddress != "" ) {
+        filteredUsers = filteredUsers.filter(user => user.emailAddress.toLowerCase().includes(this.filter.emailAddress.toLowerCase()))
+      }
+      if( this.filter.status != "" ) {
+        filteredUsers = filteredUsers.filter(user => user.status === this.filter.status)
+      }
+      return filteredUsers;
+       }
+
     }
-}
+      }
+  
 </script>
 
 <style scoped>
