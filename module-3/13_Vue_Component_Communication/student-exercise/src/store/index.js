6afd5b2bd468098452179ba3fe3bc7fe50c12410
Samantha Lemke <Sjlemke22@gmail.com>
2021-03-29 15:36:23 -0400

    lesson

--- module-3/13_Vue_Component_Communication/student-exercise/src/store/index.js
+++ module-3/13_Vue_Component_Communication/student-exercise/src/store/index.js
@@ -7,40 +7,40 @@ export default new Vuex.Store({
   state: {
     books: [
       {
-        title: "Kafka on the Shore",
-        author: "Haruki Murakami",
-        read: false,
-        isbn: "9781400079278"
+      title: "Kafka on the Shore",
+      author: "Haruki Murakami",
+      read: false,
+      isbn: "9781400079278"
       },
       {
       title: "The Girl With All the Gifts",
       author: "M.R. Carey",
       read: true,
       isbn: "9780356500157"
-
       },
-     {
-       title: "The Old Man and the Sea",
-       author: "Ernest Hemingway", 
-       read: true, 
-       isbn: "9780684830490"
+      {
+      title: "The Old Man and the Sea",
+      author: "Ernest Hemingway",
+      read: true,
+      isbn: "9780684830490"
       },
-       {
-        title: "Le Petit Prince",
-        author: "Antoine de Saint-Exupéry", 
-        read: false,
-        isbn: "9783125971400"
+      {
+      title: "Le Petit Prince",
+      author: "Antoine de Saint-Exupéry",
+      read: false,
+      isbn: "9783125971400"
       }
-    ]
+     ]  
   },
-
-
   mutations: {
     FLIP_READ_STATE(state, bookToChange) {
       bookToChange.read = ! bookToChange.read;
+    },
+    ADD_BOOK(state, book) {      // Add a review to our data array - receives the state and the new review
+      state.books.unshift(book); // Takes the new review passed and add it to the start of the array
     }
   },
   actions: {},
   modules: {},
-  strict: true
+  strict: true, 
 });
