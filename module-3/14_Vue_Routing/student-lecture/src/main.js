a8ddb6059deadc5f155f265267b3362dbfc53882
Samantha Lemke <Sjlemke22@gmail.com>
2021-03-31 19:25:13 -0400

    lesson

--- module-3/14_Vue_Routing/student-lecture/src/main.js
+++ module-3/14_Vue_Routing/student-lecture/src/main.js
@@ -1,12 +1,12 @@
-import Vue from 'vue'
-import App from './App.vue'
-import store from './store'
-import router from './router'
+import Vue from 'vue'  //Access the code to support vue
+import App from './App.vue'  //Access the code for our main page
+import store from './store'  //Access the code to support the vue data store
+import router from './router' //access the code to support Vue router
 
 Vue.config.productionTip = false
 
-new Vue({
-  store,
-  router,
-  render: h => h(App)
-}).$mount('#app')
+new Vue({    //
+  store,    //tell vue we are using the vue data store
+  router,   //tell vue we are using vue router
+  render: h => h(App) //tell vue our main page is App.vye component
+}).$mount('#app')  //tell vue to add whatever it generates to the index.html element with id='app'
